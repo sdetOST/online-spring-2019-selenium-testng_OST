40830d7ced3615db0eb8f011ff407eab51b76033
cybertekstudio <vfomiuk@gmail.com>
2019-07-16 22:20:08 -0400

    Day 12 Implicit and Explicit wait

---
+++ src/test/java/com/cybertek/tests/OfficeHours/July_15.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.OfficeHours;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class July_15 {
+
+    public static void main(String[] args) {
+//        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("MMM d, yyy")));
+//        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+//        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+
+
+
+    }
+}

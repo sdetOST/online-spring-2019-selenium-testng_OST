fa29bae28dfcea1da9fc672ce06735c61d9796fd
cybertekstudio <vfomiuk@gmail.com>
2019-07-20 17:06:56 -0400

    Day 14, Test Base, Singleton, Properties

---
+++ src/test/java/com/cybertek/tests/day14_properties_singleton_driver_test_base/LoginTests.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day14_properties_singleton_driver_test_base;
+
+import com.cybertek.utilities.ConfigurationReader;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void loginTests1(){
+        //DEMO for reading credentials from properties file
+       String username = ConfigurationReader.getProperty("username");
+       String password = ConfigurationReader.getProperty("password");
+       loginPage.login(username, password);
+    }
+}

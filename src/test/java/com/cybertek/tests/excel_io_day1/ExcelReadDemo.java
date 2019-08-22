2c78961951cf4638e54e02bb8d9299ba2922a241
cybertekstudio <vfomiuk@gmail.com>
2019-08-21 21:49:00 -0400

    excel file reading

--- src/test/java/com/cybertek/tests/excel_io_day1/ExcelReadDemo.java
+++ src/test/java/com/cybertek/tests/excel_io_day1/ExcelReadDemo.java
@@ -1,14 +1,15 @@
 package com.cybertek.tests.excel_io_day1;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExcelReadDemo {
     //to create on object that will store excel file
@@ -28,10 +29,61 @@ public class ExcelReadDemo {
     }
 
     @Test
-    public void readExcelFileRowAndColTest(){
+    public void readExcelFileRowAndColTest() {
         int rowCount = sheet.getLastRowNum();
-        System.out.println("Row count: "+rowCount);
+        System.out.println("Row count: " + rowCount);
+        //how to get number of columns?
+
+        int colCount = sheet.getRow(0).getLastCellNum();
+
+        System.out.println("Number of columns: " + colCount);
     }
+
+    @Test
+    public void readSingleCellValueTest() {
+        Row row = sheet.getRow(0);
+        Cell cell = row.getCell(0);
+        String value = cell.getStringCellValue();
+        System.out.println("Value: " + value);
+
+        String expected = "Nur-Sultan";
+        String actual = sheet.getRow(3).getCell(1).toString();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void readWholeSheetTest() {
+        int rowCount = sheet.getLastRowNum();
+        for (int i = 0; i <= rowCount; i++) {
+            String country = sheet.getRow(i).getCell(0).getStringCellValue();
+            String capital = sheet.getRow(i).getCell(1).getStringCellValue();
+            System.out.println(country + " - " + capital);
+        }
+    }
+
+    @Test
+    public void putDataIntoMapTest() {
+        //key - country
+        //value - capital
+        Map<String, String> countries = new HashMap<>();
+        int rowCount = sheet.getLastRowNum();
+        for (int i = 0; i <= rowCount; i++) {
+            String country = sheet.getRow(i).getCell(0).getStringCellValue();
+            String capital = sheet.getRow(i).getCell(1).getStringCellValue();
+            countries.put(country, capital);
+        }
+        System.out.println(countries);
+        String expected = "Moscow";
+        String actual = countries.get("Russia");
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void putDataIntoListOfMapsTest(){
+
+    }
+
 
     @AfterMethod
     public void tearDown() throws IOException {
